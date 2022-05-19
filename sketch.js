df0f47e2dd31faac88192c59ae9cca1a8424ccbf
mattcoosh <mcuschie@risd.edu>
2022-05-19 00:37:35 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,5 +1,50 @@
 $(function () {
   $("#constantin").on("click", function () {
-    $("#content").toggle();
+    $("#contentconst").toggle();
+  });
+  $("#jake").on("click", function () {
+    $("#contentjake").toggle();
+  });
+  $("#justin").on("click", function () {
+    $("#contentjust").toggle();
+  });
+  $("#scott").on("click", function () {
+    $("#contentscott").toggle();
+  });
+  $("#ray").on("click", function () {
+    $("#contentray").toggle();
+  });
+  $("#max").on("click", function () {
+    $("#contentmax").toggle();
+  });
+  $("#chenxi").on("click", function () {
+    $("#contentchenxi").toggle();
+  });
+  $("#sage").on("click", function () {
+    $("#contentsage").toggle();
+  });
+  $("#njari").on("click", function () {
+    $("#contentnjari").toggle();
+  });
+  $("#jessie").on("click", function () {
+    $("#contentjessie").toggle();
+  });
+  $("#joel").on("click", function () {
+    $("#contentjoel").toggle();
+  });
+  $("#rudy").on("click", function () {
+    $("#contentrudy").toggle();
+  });
+  $("#nina").on("click", function () {
+    $("#contentnina").toggle();
+  });
+  $("#isabelle").on("click", function () {
+    $("#contentisabelle").toggle();
+  });
+  $("#alana").on("click", function () {
+    $("#contentalana").toggle();
+  });
+  $("#perri").on("click", function () {
+    $("#contentperri").toggle();
   });
 });
