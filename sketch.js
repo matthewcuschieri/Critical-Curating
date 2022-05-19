041c6d22af2ce12a87279c0838a1b95d6a016187
mattcoosh <mcuschie@risd.edu>
2022-05-19 00:04:30 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,13 +1,5 @@
-$("#cc").html(
-  $("#cc")
-    .text()
-    .split("")
-    .map((a) => `<span class="cc">${a}</span>`)
-);
-
-$("#secCC").html(
-  $("#secCC")
-    .text()
-    .split("")
-    .map((a) => `<span class="secCC">${a}</span>`)
-);
+$(function () {
+  $("#constantin").on("click", function () {
+    $("#content").toggle();
+  });
+});
